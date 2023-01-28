07913f297627c2ff6a6a09b0c72801ad95ae04ab
Kaden Helmka <107013775+CosineDigital@users.noreply.github.com>
2023-01-28 12:51:05 -0600

    Update util_bill.py

--- util_bill.py
+++ util_bill.py
@@ -1,29 +1,39 @@
 import pandas as pd
 import numpy as np
 
-
+# read a single csv file
+# store data from csv file in a panda dataframe
+# each row is vec2 topleft, vec2 bottomright, value
 def read_ocr(file):
     '''read one orc file into pandas dataframe'''
 
-    data = {"x1":[], "y1":[],"x2":[], "y2":[],"x3":[], "y3":[],"x4":[], "y4":[], "value":[]}
+    data = {"tl":[], "br":[], "value":[]};
+
     with open(file) as f:
          lines = f.readlines()
 
          for line in lines:
             row = line.split(',', 8)
 
-            data['x1'].append(row[0])
-            data['y1'].append(row[1])
-            data['x2'].append(row[2])
-            data['y2'].append(row[3])
-            data['x3'].append(row[4])
-            data['y3'].append(row[5])
-            data['x4'].append(row[6])
-            data['y4'].append(row[7])
-            data['value'].append(row[8])
+            data['tl'].append(vec2(float(row[0]), float(row[1])));
+            data['br'].append(vec2(float(row[4]), float(row[5])));
+            data['value'].append(row[8]);
 
     return pd.DataFrame(data)
 
+# read the User.csv file
+# @return: a tuple of two dataframes, the first being the test data
+# and the second being the actual data
+def readUserFile():
+
+    users_csv = pd.read_csv(f"./Users.csv");
+    user_data = users_csv.iloc[:, -4:];
+    test_data = users_csv.iloc[:, :2];
+
+    return user_data, test_data;
+
+# calculate accuracy
+# return float [0, 1]
 def calculate_accuracy(df):
     '''
     calculate the fraction of correction prediction
@@ -32,4 +42,15 @@ def calculate_accuracy(df):
     where "prediction" is the predicted document name
     '''
     num_correct = np.sum(df["prediction"] == df["documentid"])
+
     return float(num_correct/len(df))
+
+
+
+# structure for storing a 2d point
+class vec2:
+    x = 0;
+    y = 0;
+    def __init__(self, _x, _y):
+        self.x = _x;
+        self.y = _y;
