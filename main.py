2c003a461d67eee403a8e433fba64c1ac08926a2
Kaden Helmka <107013775+CosineDigital@users.noreply.github.com>
2023-01-28 17:08:10 -0600

    Update main.py

--- main.py
+++ main.py
@@ -10,9 +10,7 @@ from util_bill import *
 data = readUserFile();
 
 userData = data[0];
-
 testData = data[1];
-
 
 # get the data from all of
 root = pathlib.Path(__file__).parent.resolve();
@@ -30,12 +28,39 @@ for i in range(len(filesList)):
 
     df = read_ocr(path);
     count += (len(df.index));
-    print(df);
+    #print(fileName);
+    #print(df);
 
     dfDict[fileName.split('.')[0]] = df;
 
+# now go through the user dataset and determine which one is best
+numRows = userData.shape[0];
+numCols = userData.shape[1];
 
-print(count / 562.0);
+for r in range(numRows):
+    # for each entry in userdata
+
+    row = userData[r, :];
+
+    #amount,date,vendor_name,vendor_address
+    amount = row[0];
+    date = row[1];
+    vendor_name = row[2];
+    vendoor_address = row[3];
+
+    matchingFile = "";
+
+    # insert algorithm to match these values to each file and to each row
+
+
+
+
+
+
+
+
+
+
 
 
 
